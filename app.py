59f5006bccb2deb18a0a6128aa400c5961e97045
Johan Cañas <jxexcxo@gmail.com>
2021-01-25 20:42:32 -0500

    currency data

--- app.py
+++ app.py
@@ -3,10 +3,11 @@ import feedparser
 import json
 import urllib.request,urllib.parse, urllib.error
 import requests
+from secrets import WEATHER_API_KEY, CURRENCY_APY_KEY
 
 
 app = Flask(__name__)
-API_KEY = "da0f78399d23003917b1e6e2a068e212"
+
 
 RSS_FEEDS =  {
     'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
@@ -17,7 +18,9 @@ RSS_FEEDS =  {
 
 DEFAULTS = {
     'publication': 'bbc',
-    'city': 'Bogota'
+    'city': 'Bogota',
+    'currency_from': 'USD',
+    'currency_to': 'COP'
 }
 
 @app.route('/', methods=['GET', 'POST'])
@@ -31,7 +34,20 @@ def home():
         city = DEFAULTS['city']
     city = city.replace(' ', '+')
     weather = get_weather(city)
-    return  render_template('index.html', articles=articles, weather=weather)
+    currency_from = request.args.get('currency_from')
+    if not currency_from:
+        currency_from = DEFAULTS['currency_from']
+    currency_to = request.args.get('currency_to')
+    if not currency_to:
+        currency_to = DEFAULTS['currency_to']
+    rate, currencies = get_rate(currency_from, currency_to)
+    return  render_template('index.html', 
+                                articles=articles, 
+                                weather=weather,
+                                currency_from=currency_from,
+                                currency_to=currency_to,
+                                rate=rate,
+                                currencies=sorted(currencies))
 
 def get_news(query):
     #query = request.args.get("publication")
@@ -46,15 +62,15 @@ def get_news(query):
 
 def get_weather(city):
     query = urllib.parse.quote(city)
-    print(city)
+    #print(city)
     # 1. get the api url with the api key
-    api_url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid={API_KEY}"
+    api_url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid={WEATHER_API_KEY}"
     # 2. fetching the api url
     url = urllib.request.urlopen(api_url)
-    print(url)
+    #print(url)
     # 3. read the http response
     data = url.read()
-    print(data)
+    #print(data)
     # 4. convert the response to json
     parsed = json.loads(data)
     weather = None
@@ -62,10 +78,20 @@ def get_weather(city):
         weather = {"description":
                     parsed["weather"][0]["description"],
                     "temperature":parsed["main"]["temp"],
-                    "city":parsed["name"]
+                    "city":parsed["name"],
+                    "country": parsed['sys']['country']
                     }
     #print(weather)
     return weather
+
+def get_rate(frm, to):
+    currency_url = f"https://openexchangerates.org//api/latest.json?app_id={CURRENCY_APY_KEY}"
+    data = urllib.request.urlopen(currency_url).read()
+    data = json.loads(data).get('rates')
+    frm_rate = data.get(frm.upper())
+    to_rate = data.get(to.upper())
+    return (to_rate/frm_rate, data.keys())
+
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
